9f2e3fbde1ed81e6b78a50e36fdf530305022a98
Elliot <bendavidelliot9@gmail.com>
2024-07-04 16:52:11 +0200

    correction imports

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, jsonify, flash
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from thefuzz import process
 from currency_converter import CurrencyConverter
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
 
 EXCEL_FILE = 'excelBE.xlsx'
 df = pd.read_excel(EXCEL_FILE)
